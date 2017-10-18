6ebbe2dbc502a25e31b8eebff77a52375ca430cd
Egor Stambakio <stambakio@scand.com>
2017-10-18 16:37:44 +0300

    Fixed gh-pages url

--- config/client.js
+++ config/client.js
@@ -1,5 +1,5 @@
 // basename for react Router
 // should have a slash at the start and not have a slash at the end
 
-// export const baseUrl = "/react-crudeditor/branches/serverless";
-export const baseUrl = "/";
+export const baseUrl = "/react-crudeditor/branches/serverless";
+// export const baseUrl = "/";

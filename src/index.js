bcff11df289a1593db7408db31a072415a11c277
Egor Stambakio <stambakio@scand.com>
2017-11-27 16:13:01 +0300

    ...

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-const crud = require('./crudeditor-lib')
-
-module.exports = {
-  default: crud.default,
-  VIEW_SEARCH: crud.VIEW_SEARCH,
-  VIEW_CREATE: crud.VIEW_CREATE,
-  VIEW_EDIT: crud.VIEW_EDIT,
-  VIEW_SHOW: crud.VIEW_SHOW,
-};
+module.exports = require('./crudeditor-lib');

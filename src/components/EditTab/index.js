c283242f4c87f3f754e28face8c30ec3094c70fa
Egor Stambakio <stambakio@scand.com>
2017-11-29 13:23:28 +0300

    Deleted Revisions button placeholder.

--- src/components/EditTab/index.js
+++ src/components/EditTab/index.js
@@ -157,12 +157,12 @@ export default class EditTab extends React.PureComponent {
       )
     }
 
-    if ([VIEW_EDIT, VIEW_SHOW].indexOf(viewName) > -1) {
-      buttons.push(
-        <Button disabled={true} key="Revisions">
-          {i18n.getMessage('crudEditor.revisions.button')}
-        </Button>)
-    }
+    // if ([VIEW_EDIT, VIEW_SHOW].indexOf(viewName) > -1) {
+    //   buttons.push(
+    //     <Button disabled={true} key="Revisions">
+    //       {i18n.getMessage('crudEditor.revisions.button')}
+    //     </Button>)
+    // }
 
     if ([VIEW_CREATE, VIEW_EDIT].indexOf(viewName) > -1 && permissions.create) {
       buttons.push(

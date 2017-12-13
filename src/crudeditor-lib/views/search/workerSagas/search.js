fb1fdc27a78f63517197b05a51fc169356b1a10d
Andrei Mourzenkov <amourzenkov-sc@users.noreply.github.com>
2017-12-13 16:07:30 +0300

    Correcting comments in Search Saga

--- src/crudeditor-lib/views/search/workerSagas/search.js
+++ src/crudeditor-lib/views/search/workerSagas/search.js
@@ -92,7 +92,7 @@ export default function*({
     order = order || currentOrder;
     max = max || currentMax;
 
-    // Reset offset to 0 with new sortField, sortOrder, pageMax or filter.
+    // Reset offset to 0 with new sortField, pageMax or filter.
     offset = sort === currentSort &&
       max === currentMax &&
       isEqual(

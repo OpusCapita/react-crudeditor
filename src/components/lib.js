b107e14cceaf310e62fcc56f1ae9755e82257a72
Andrei Mourzenkov <mourzenkov@scand.com>
2018-01-08 15:03:36 +0300

    Test corrections

--- src/components/lib.js
+++ src/components/lib.js
@@ -2,14 +2,14 @@ const titleCase = str => typeof str === 'string' ?
   str.charAt(0).toUpperCase() + str.slice(1).replace(/[^A-Z](?=[A-Z])/g, '$&\u00A0') :
   str;
 
-export function getModelMessage(i18n, key, default) {
+export function getModelMessage(i18n, key, defaultMessage) {
 // If i18n doesn't find a message by key, it returns the key itself.
-// Optinal third argument defines default for such a case.
+// Optinal third argument defines default message for such a case.
 
   const message = i18n.getMessage(key);
 
   return message.slice(-key.length) === key && arguments.length === 3 ?
-    titleCase(default) :
+    titleCase(defaultMessage) :
     message;
 }
 

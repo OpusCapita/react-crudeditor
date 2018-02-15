7535280e40e5b584ba865fe77fb3cab37b010af4
OCNET\saloaap1 <aappo.salo@opuscapita.com>
2018-02-15 15:05:23 +0200

    Fixed a typo, added some more UI string fixes.

--- src/crudeditor-lib/i18n/fi.js
+++ src/crudeditor-lib/i18n/fi.js
@@ -18,15 +18,15 @@ const common = {
   "crudEditor.message.ajax.loading": "Ole hyvä ja odota…",
   "crudEditor.noAssociationEntriesFound.message": "Syötteet puuttuvat. Voit {1} uuden syötteen.",
   "crudEditor.noItemsSelected.alert": "Rivejä ei ole valittu!",
-  "crudEditor.objectDeleteFailed.message": "Objektin poistaminen epäonnistui, se voi olla jo käytössä.",
-  "crudEditor.objectDeleted.message": "Objekti poistettu.",
-  "crudEditor.objectDuplicated.message": "Objekti kopioitu.",
-  "crudEditor.objectSaveFailed.message": "Objektin tallentaminen epäonnistui.",
-  "crudEditor.objectSaved.message": "Objekti luotu.",
-  "crudEditor.objectUpdated.message": "Objekti päivitetty.",
-  "crudEditor.objectsDeleteFailed.message": "Objektien {0} poistaminen epäonnistui, koska ne ovat jo käytössä.",
-  "crudEditor.objectsDeleteIsNoAllowed.message": "Et voi poistaa joitakin objekteja turvallisuusrajoitteista johtuen.",
-  "crudEditor.objectsDeleted.message": "Objektit {labels} poistettu.",
+  "crudEditor.objectDeleteFailed.message": "Rivin poistaminen epäonnistui, se voi olla jo käytössä.",
+  "crudEditor.objectDeleted.message": "Rivi poistettu.",
+  "crudEditor.objectDuplicated.message": "Rivi kopioitu.",
+  "crudEditor.objectSaveFailed.message": "Rivin tallentaminen epäonnistui.",
+  "crudEditor.objectSaved.message": "Rivi luotu.",
+  "crudEditor.objectUpdated.message": "Rivi päivitetty.",
+  "crudEditor.objectsDeleteFailed.message": "Rivien {0} poistaminen epäonnistui, koska ne ovat jo käytössä.",
+  "crudEditor.objectsDeleteIsNoAllowed.message": "Et voi poistaa joitakin rivejä turvallisuusrajoitteista johtuen.",
+  "crudEditor.objectsDeleted.message": "Rivit {labels} poistettu.",
   "crudEditor.refresh.button": "Päivitä",
   "crudEditor.reset.button": "Tyhjennä",
   "crudEditor.revisions.button": "Revisiot",
@@ -45,10 +45,10 @@ const common = {
   "crudEditor.export.button": "Vie",
   "crudEditor.found.items.message": "{count} hakutulosta l\u00F6ytyi",
   "crudEditor.range.from": "l\u00E4htien",
-  "crudEditor.range.to": "asti"
+  "crudEditor.range.to": "asti",
   "crudEditor.confirm.action": "Vahvista",
-  "crudEditor.search.showSearchForm": "Näytä hakukaavake",
-  "crudEditor.search.hideSearchForm": "Piilota hakukaavake"
+  "crudEditor.search.showSearchForm": "Näytä hakukentät",
+  "crudEditor.search.hideSearchForm": "Piilota hakukentät"
 }
 /* eslint-enable max-len */
 

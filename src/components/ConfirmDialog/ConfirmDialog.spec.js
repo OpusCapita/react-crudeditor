9cd5075f63c5415d717de70f69d00e2d7f06200f
Dmitriy Sanko <dmitriy.sanko@opuscapita.com>
2021-09-09 15:37:46 +0300

    Migrate to React 16.14.0

--- src/components/ConfirmDialog/ConfirmDialog.spec.js
+++ src/components/ConfirmDialog/ConfirmDialog.spec.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Enzyme from "enzyme";
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 import { expect } from 'chai';
 import sinon from 'sinon';
 import ConfirmDialog from './';
